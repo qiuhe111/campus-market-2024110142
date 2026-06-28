91c670d9611308a20f4300a5e8ba26ed71d3a801
qiuhe111 <2486439554@qq.com>
2026-06-28 09:28:32 +0800

    day2: add multi-page layout and router navigation

---
+++ src/views/DetailView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const id = route.params.id
+</script>
+
+<template>
+  <div>
+    <h1>详情页</h1>
+    <p>商品 ID：{{ id }}</p>
+    <router-link to="/list">← 返回列表</router-link>
+  </div>
+</template>
+
+<style scoped></style>
